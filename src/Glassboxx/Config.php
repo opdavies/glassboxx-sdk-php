e5268e9707293d257816dc0b0ab6946349933acf
Oliver Davies <oliver@oliverdavies.uk>
2020-05-29 13:26:26 +0100

    Add the Config class
    Add a Config class for storing configuration values such as the
    Glassboxx username, password and vendor ID.

---
+++ src/Glassboxx/Config.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Opdavies\Glassboxx;
+
+class Config
+{
+    /** @var int $vendorId */
+    private $vendorId;
+
+    /** @var string $username */
+    private $username;
+
+    /** @var string $password */
+    private $password;
+
+    public function __construct(
+		int $vendorId,
+        string $username,
+        string $password
+	) {
+        $this->password = $password;
+        $this->username = $username;
+        $this->vendorId = $vendorId;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getVendorId(): int
+    {
+        return $this->vendorId;
+    }
+}
---
+++ tests/Glassboxx/ConfigTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Opdavies\Glassboxx\Tests\Glassboxx;
+
+use Opdavies\Glassboxx\Config;
+use PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+    public function testThatGettersReturnExpectedValues(): void
+    {
+        $config = new Config(
+            12345,
+            'opdavies',
+            'secret'
+        );
+
+        $this->assertSame(12345, $config->getVendorId());
+        $this->assertSame('opdavies', $config->getUsername());
+        $this->assertSame('secret', $config->getPassword());
+    }
+}
