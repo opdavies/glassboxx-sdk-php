b042256e45453a983039746152a3833bf02413c0
Oliver Davies <oliver@oliverdavies.uk>
2020-05-30 03:18:43 +0100

    Declare strict types in existing files

--- src/Glassboxx/Request/AbstractRequest.php
+++ src/Glassboxx/Request/AbstractRequest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Opdavies\Glassboxx\Request;
 
 abstract class AbstractRequest
--- src/Glassboxx/Request/AuthTokenRequest.php
+++ src/Glassboxx/Request/AuthTokenRequest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Opdavies\Glassboxx\Request;
 
 use Opdavies\Glassboxx\Config;
